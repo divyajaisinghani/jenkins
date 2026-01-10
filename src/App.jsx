6f0d58fb13dc0eeaffacece38fd99b85dc6e3590
workdivya22 <workdivya22@gmail.com>
2026-01-10 18:31:17 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ function App() {
   return (
     <div>
       Hello World, my name is Divya Jaisinghani and I am learning Devops
-      This is new line!!
-      
+      This is new line!
     </div>
   );
 }
